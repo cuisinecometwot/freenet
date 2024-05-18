d4d3ab9760995d6f1437c227d3e7f2710e860fe7
Tran Trung Anh <trantrunganh279@gmail.com>
2024-05-18 09:18:42 +0700

    Model

---
+++ src/Model/taishou/Customer.java
@@ -0,0 +1,63 @@
+package Model.taishou;
+
+public class Customer {
+    private String name;
+    private String email;
+    private String phoneNum;
+    private int balance;
+    private String username;
+    private String password;
+
+    public Customer(){
+
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
