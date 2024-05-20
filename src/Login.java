ce165bd2cf7f1e63baf5084744f17c4b080101ae
Duy <vuducduy1012@gmail.com>
2024-05-21 02:06:01 +0700

    Refactor resources

--- src/Login.java
+++ src/Login.java
@@ -7,7 +7,7 @@ import javafx.stage.Stage;
 public class Login extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/View2/fxml/Login.fxml"));
         stage.setTitle("Log in");
         stage.setScene(new Scene(root));
         stage.setResizable(false);
