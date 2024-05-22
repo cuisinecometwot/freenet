d7860a97e086f8e953fb4c99be2c677db3da9be2
Duy <vuducduy1012@gmail.com>
2024-05-22 09:58:20 +0700

    Create overflow (add StaffMenu controller)

---
+++ src/Controller/StaffMenu.java
@@ -0,0 +1,24 @@
+package Controller;
+
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class StaffMenu implements Initializable {
+    public Label lblName;
+    public Button btnComputers;
+    public Button btnUsers;
+    public Button btnStaff;
+    public Button btnOrders;
+    public Button btnSchedules;
+    public Button btnLogout;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
